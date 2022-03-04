a372b512813237327406042aaa39494c2f82cb21
Zi Page <59000769+zipage@users.noreply.github.com>
2022-03-03 21:36:10 -0700

    Add files via upload

---
+++ intro-component-with-signup-form-master/action_page.php
@@ -0,0 +1,17 @@
+<?php
+// define variables and set to empty values
+$fname = $lname = $email = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $fname = test_input($_POST["fname"]);
+  $lname = test_input($_POST["lname"]);
+  $email = test_input($_POST["email"]);
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+?>
